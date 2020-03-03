67681e341bd8d5d655e500a1584ef1f8c62b5f79
tantian1118 <270451831@qq.com>
2020-03-03 11:17:25 +0800

    123123

--- java/demo.java
+++ java/demo.java
@@ -1,3 +1,7 @@
-public static void main(String[] args){
-    System.out.println("hello world")
-        }
+
+public class demo{
+    public static void main(String[] args) {
+
+        System.out.println("123");
+    }
+}
